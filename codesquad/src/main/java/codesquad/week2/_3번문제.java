12c73882de067f8d659b122db17bf90d153c009a
leegyeongwhan <leekhy02@naver.com>
2022-11-08 22:39:22 +0900

    perf(3번 문제 로직 수정): 최단 경로를 찍기 위해 로직수정

--- codesquad/src/main/java/codesquad/week2/_3번문제.java
+++ codesquad/src/main/java/codesquad/week2/_3번문제.java
@@ -12,7 +12,6 @@ import java.util.*;
 //00100
 //00111
 
-
 public class _3번문제 {
     static int[][] map;
     static int[][] dfsMap;
@@ -22,8 +21,7 @@ public class _3번문제 {
     static int[] dirY = {0, 1, 0, -1};
     static int N;
     static int M;
-    static Stack<Point> stack;
-    static Boolean exitFlag = false;
+    public static String path = "";        // 이동경로
 
 
     public static void main(String[] args) throws IOException {
@@ -43,65 +41,79 @@ public class _3번문제 {
                 map[i][j] = input.charAt(j) - 48;
             }
         }
+        path = "";
+        System.out.println("이동한거리는= " + bfs() + " " + path);
 
-
-        bfs(0, 0);
-
-        for (int[] ints : dfsMap) {
-            for (int anInt : ints) {
-                System.out.print(anInt + " ");
-            }
-            System.out.println();
-        }
-
-        while (!stack.isEmpty()) {
-
-            Point point = stack.pop();
-            System.out.print(" (" + point.getX() + " " + point.getY() + ") , ");
-        }
     }
 
-    private static void bfs(int x, int y) {
+    private static int bfs() {
+
+        int ret = 0;
+        int curX = 0;        // 현재 row
+        int curY = 0;        // 현재 col
+        int curDist = 1;    // 현재 이동한 거리
+        String curPath = "";    // 현재까지 이동경로
         Queue<Point> queue = new LinkedList<>();
-        stack = new Stack<>();
-        queue.offer(new Point(x, y));
-        visit[x][y] = true; //지난 곳은 1로 바꿔준다
+        queue.offer(new Point(0, 0, curDist, curPath));
 
         while (!queue.isEmpty()) {
-            Point tmpPoint = queue.poll();
-            //     Point peek = stack.peek();
-//            if (dfsMap[tmpPoint.getX()][tmpPoint.getY()] == dfsMap[peek.getX()][peek.getY()]) {
-//
-//            }
+            Point point = queue.poll();
+            curX = point.getX();
+            curY = point.getY();
+            curDist = point.getDist();
+            curPath = point.getPath();
+            path = curPath;
+            ret = curDist; //거리
+            visit[curX][curY] = true;
 
-            stack.add(tmpPoint);
+
+            // 목적지에 도착하면 루핑 종료
+            if (curX == N - 1 && curY == M - 1) {
+                break;
+            }
+
+            // 위로 갈수 있으면
+            if (curX - 1 >= 0 && !visit[curX - 1][curY]) {
+                queue.add(new Point(curX - 1, curY, curDist + 1, curPath));
+            }
+            // 아래로 갈수 있으면
+            if (curX + 1 < N && !visit[curX + 1][curY]) {
+                queue.add(new Point(curX + 1, curY, curDist + 1, curPath));
+            }
+            // 왼쪽으로 갈수 있으면
+            if (curY - 1 >= 0 && !visit[curX][curY - 1]) {
+                queue.add(new Point(curX, curY - 1, curDist + 1, curPath));
+            }
+            // 오른쪽으로 갈수 있으면
+            if (curY + 1 < M && !visit[curX][curY + 1]) {
+                queue.add(new Point(curX, curY + 1, curDist + 1, curPath));
+            }
+        }
+        return ret;
+    }
+
+
+    public static boolean dfs(int x, int y, boolean[][] clone) {
+
+        if (x == N - 1 && y == M - 1) {
+            return true;
+            // 종료
+        } else {
 
             for (int i = 0; i < 4; i++) {
-                int nx = tmpPoint.getX() + dirX[i];
-                int ny = tmpPoint.getY() + dirY[i];
+                int nx = x + dirX[i];
+                int ny = y + dirY[i];
 
-                if (nx < 0 || ny < 0 || nx > N - 1 || ny > M - 1 || visit[nx][ny]) {
-                    continue;
-                }
-//
-//                if (nx == N - 1 && ny == N - 1) {
-//                    exitFlag = true;
-//                    return;
-//                }
-                // 아니라면 더 가기
-//                bfs(nx, ny);
-//                if (!exitFlag) {
-//                   stack.pop(); // 되돌아가는 좌표 pop
-//                }
-
-                if (map[nx][ny] == 1 && !visit[nx][ny]) {
-                    visit[nx][ny] = true;
-                    // 0,0  1,0 0,1
-                    queue.offer(new Point(nx, ny));
-                    dfsMap[nx][ny] = dfsMap[tmpPoint.getX()][tmpPoint.getY()] + 1;
+                if (nx >= 0 && nx <= N - 1 && ny >= 0 && ny <= M - 1 && map[nx][ny] == 1) {
+                    if (!clone[nx][ny]) {
+                        clone[nx][ny] = true;
+                        dfs(nx, ny, clone);
+                        clone[nx][ny] = false;
+                    }
                 }
             }
         }
+        return false;
     }
 
 }
@@ -109,10 +121,19 @@ public class _3번문제 {
 class Point {
     private int x;
     private int y;
+    private int dist;
+    String path;
 
-    public Point(int x, int y) {
+    public Point(int x, int y, int dist, String path) {
         this.x = x;
         this.y = y;
+        this.dist = dist;
+
+        if ("".equals(path)) {
+            this.path = "(" + x + ", " + y + ")";
+        } else {
+            this.path = path + " -> " + "(" + x + ", " + y + ")";
+        }
     }
 
     public int getX() {
@@ -122,5 +143,12 @@ class Point {
     public int getY() {
         return y;
     }
-}
 
+    public int getDist() {
+        return dist;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
